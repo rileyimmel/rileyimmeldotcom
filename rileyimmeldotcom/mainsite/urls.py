2b645377391e21f8f59f5139f92bf6271947873f
rileyimmel <ypq8st@virginia.edu>
2023-09-22 19:00:10 -0400

    Add page for my resume

--- rileyimmeldotcom/mainsite/urls.py
+++ rileyimmeldotcom/mainsite/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index.as_view(), name="index"),
+    path('', views.index.as_view(), name="index"),
+    path('resume/', views.pdf_view, name="resume"),
 ]
--- rileyimmeldotcom/mainsite/views.py
+++ rileyimmeldotcom/mainsite/views.py
@@ -1,7 +1,10 @@
+import os
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse, Http404
 from django.template import loader
 from django.views import generic
+
 
 # Create your views here.
 
@@ -11,3 +14,9 @@ class index(generic.ListView):
     def get_queryset(self):
         return {}
 
+
+def pdf_view(request):
+    with open('./mainsite/static/resume.pdf', 'rb') as pdf:
+        response = HttpResponse(pdf.read(), content_type='application/pdf')
+        response['Content-Disposition'] = 'inline;filename=mypdf.pdf'
+        return response
