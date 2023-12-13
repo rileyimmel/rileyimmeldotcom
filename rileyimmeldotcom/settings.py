f3b0c2aef82a6ec3526f31800abe0d202eaedb50
rileyimmel <ypq8st@virginia.edu>
2023-12-13 05:19:37 -0500

    add hosts

--- rileyimmeldotcom/settings.py
+++ rileyimmeldotcom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.254', "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ['192.168.1.254', "127.0.0.1", "localhost", "rileyimmel.com", "www.rileyimmel.com", ".rileyimmel.com"]
 
 
 # Application definition
